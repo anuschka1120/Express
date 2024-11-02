7e0ca569c7504c6cf4f1024e7bb7317c31078be6
anuchka1120 <2001015145ra@cyber-u.ac.jp>
2024-11-02 11:45:37 +0900

    Update app.js, hello.js, and hello.jade for /test route testing2

--- routes/hello.js
+++ routes/hello.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  console.log("Inside /hello route"); // ログメッセージをわかりやすく変更
-  res.render('hello', { title: 'Simple Test Page' }); // hello.jade をレンダリング
+  res.render('hello', { title: 'Hello Express 2' });
 });
 
 module.exports = router;
