9a5645d14e81e9a8aef58f2d65720f94428383cb
anuchka1120 <2001015145ra@cyber-u.ac.jp>
2024-11-09 18:09:57 +0900

    Add notes.js

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var notesRouter = require('./routes/notes');
 
 var app = express();
 
@@ -15,11 +18,13 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/hello', helloRouter); // helloRouter のみ使用
+app.use('/', indexRouter); 
+app.use('/users', usersRouter); 
+app.use('/hello', helloRouter); 
+app.use('/notes', notesRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
