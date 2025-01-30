692e470734d69e2177507fb96349f7d6e00ad6c9
anuchka1120 <2001015145ra@cyber-u.ac.jp>
2025-01-30 11:31:10 +0900

    8notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,16 @@
-var express= require('express');
-var router= express.Router();
+varexpress= require('express');
+varrouter= express.Router();
 // 接続情報を設定
 const{ MongoClient} = require("mongodb");
-const uri= "mongodb+srv://anuschka:**********@cluster0.bv9y7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri);
+consturi= "mongodb+srv://anuschka1120:o0euPBV86ZbeY28A@cluster0.h5ogi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+constclient= newMongoClient(uri);
 router.get('/', async(req, res) =>{
 // データベース、コレクションを指定
-const database= client.db('notes');
-const notes= database.collection('notes');
+constdatabase= client.db('notes');
+constnotes= database.collection('notes');
 // idが１のドキュメントを取得
-const query= { id:2};
-const note= await notes.findOne(query);
+constquery= { id:2};
+constnote= awaitnotes.findOne(query);
 res.json(note);
 })
 module.exports= router;
