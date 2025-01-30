e027c1a5217421391acde974495064cc1c9bff45
anuschka1120 <2001015145ra@cyber-u.ac.jp>
2025-01-30 14:28:45 +0900

    Update notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express= require('express');
 var router= express.Router();
 // 接続情報を設定
 const{ MongoClient} = require("mongodb");
-const uri= "mongodb+srv://anuschka1120:o0euPBV86ZbeY28A@cluster0.h5ogi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri= "mongodb+srv://anuschka1120:パスワード@cluster0.h5ogi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client= new MongoClient(uri);
 router.get('/', async(req, res) =>{
 // データベース、コレクションを指定
